2a0c8f1f5e8a11733b06bd83215b54564a5fb430
Devraux <krzysztof.plonka2@onet.pl>
2024-04-02 21:35:24 +0200

    some code 'cleaning'

--- menu/menu.h
+++ menu/menu.h
@@ -1,13 +1,8 @@
+#ifndef _menu_
+#define _menu_
 #include "pico/stdlib.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-struct menu 
-{
-    char string_1;
-    char string_2;
-    struct menu* next;
-};
 
-struct menu* menu_val_init();
-void print_linked_list(struct menu *p);
+#endif
